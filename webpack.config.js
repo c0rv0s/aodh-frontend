1888d823fc639772b4064dd8301415e9a19b3edc
Nate <inathan.m@gmail.com>
2018-10-23 01:04:11 -0700

    cors

--- webpack.config.js
+++ webpack.config.js
@@ -48,10 +48,11 @@ module.exports = {
     },
     watchOptions: { aggregateTimeout: 300, poll: 1000 },
     headers: {
-			"Access-Control-Allow-Origin": "*",
-			"Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Credentials": "true",
+	"Access-Control-Allow-Methods": "ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL",
+	"Access-Control-Allow-Headers": "Overwrite, Destination, Content-Type, Depth, User-Agent, Translate, Range, Content-Range, Timeout, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, Location, Lock-Token, If",
+	"Access-Control-Expose-Headers": "DAV, content-length, Allow",
     },
   },
   module: {
